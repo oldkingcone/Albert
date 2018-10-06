afb8f864d61618f25aa8446c4d257cec216d0793
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-10-05 21:31:02 -0700

    Update Albert.py

--- Albert.py
+++ Albert.py
@@ -1,6 +1,8 @@
 #/usr/bin/env python3 
 # -*- coding: utf-8 -*-
 try:
+    import sched
+    import random
     import urllib
     from urllib.request import urlopen
     import http.client
@@ -14,15 +16,18 @@ try:
     import time
     import os
     import platform
-    import time
+    from time import sleep
     from termcolor import cprint
+    import scapy.all.ICMP
+    from scapy.all import sr, srp, IP, UDP, ICMP, TCP, ARP, Ether
+    import dpkt
 except (ImportError) as e:
     print("Something is terribly wrong:\n->{}".format(e))
 
-api = shodan.Shodan(apikey)    
+api = shodan.Shodan(apikey)
 def albert_faces():
     alberts = ''
-    albert = random.randint(1, 3)
+    albert = random.randint(1, 6)
     if albert == 1:alberts = "./albert_face.txt"
     if albert == 2:alberts = "./albert_face_2.txt"
     if albert == 3: alberts = "./fat_albert_3"
@@ -32,12 +37,12 @@ def albert_faces():
     face = open(alberts, "r")
     lulz = face.readlines()
     for line in lulz:
-        cprint(line.strip("\n", 'green')
-        time.sleep(0.5)
-    time.sleep(0.5)
-    cprint("Loading The King Himself Hopefully He Left You Some Exploits....", 'green')
-    time.sleep(0.5)
-    cprint("Gr33ts: Chef Gordon, Root, Johnny 5", 'green')
+        cprint(line.strip("\n"), 'green')
+        sleep(0.5)
+    sleep(0.5)
+    cprint("Loading The King Himself Hopefully He Left You Some Exploits....", 'red')
+    sleep(0.5)
+    cprint("Gr33ts: Chef Gordon, Root, Johnny 5", 'red')
 
 test = platform.system()
 if test == 'Windows': clear = 'cls'
@@ -46,9 +51,13 @@ elif test == 'Java': clear = 'clear'
 elif test == '':
     print("[ ! ] Exiting, you have an unknown system! [ ! ] ")
     sys.exit(1)
+
 def progress_bar(duration):
     for i in tqdm(range(int(duration))):
         time.sleep(1)
+
+api = shodan.Shodan(apikey)
+
 
 def write_file(line):
     with open('hosts_list', 'at') as f:
@@ -89,7 +98,6 @@ def list_reject(target = ''):
         print('[✘] Errpr: %s' % e)
         option = input('[*] Shieeeet you wanna chagne that API Key? <Y/n>: ').lower()
         if option.startswith('y'):
-            os.system(clear)
             file = open('api.py', 'w')
             SHODAN_API_KEY = input('[*] Hey! Hey! Hey! Enter A Valid Shodan.io API Key: ')
             oppsie = ["apikey= ", "\"",  str(SHODAN_API_KEY), "\""]
@@ -106,6 +114,35 @@ def nmapScan(tgtHost, tgtPort):  # Nmap function created
     print("[ ! ]  {}\n TCP: {} \n UP/DOWN: {}\n".format(tgtHost, tgtPort, state))
     return False
 
+def scapy_selection(scan_hosts):
+    RUN_FREQUENCY = 10
+
+    scheduler = sched.scheduler(time.time, time.sleep)
+    global scheduler
+    scheduler.enter(RUN_FREQUENCY, 1, detect_inactive_hosts, (scan_hosts, ))
+    inactive_hosts = []
+    try:
+        ans, unans = sr(IP(dst=scan_hosts)/ICMP(), retry=0, timeout=1)
+        ans.summary(lambda r : r.sprintf("%IP.src% is alive and well!"))
+        for inactive in unans:
+            print("{} is inactive..".format(inactive.dst))
+
+        print("Total: {} hosts are inactive".format(len(inactive_hosts)))
+    except KeyboardInterrupt:
+        exit(0)
+    return False
+def subnet_discover(ip):
+    import netaddr
+    question = netaddr.IPAddress(ip).reverse_dns()
+    print("{}".format(question))
+    print("{}".format(netaddr.IPNetwork.cidr(ip)))
+    print("Netmask/HostMask:\n{}\n{}\n{}\n{}\n{}\n{}".format(netaddr.IPNetwork.is_multicast(ip),
+                                             netaddr.IPNetwork.is_private(ip)),
+                                             netaddr.IPNetwork(ip).netmask(),
+                                             netaddr.IPNetwork(ip).broadcast(),
+                                             netaddr.IPNetwork(ip).hostmask(),
+                                             netaddr.IPNetwork(ip).is_multicast())
+
 if __name__ == '__main__':
     #@todo bring in a honeypot detection routine.
     #@todo a way to avoid docker containers like the plague.
@@ -114,13 +151,14 @@ if __name__ == '__main__':
     #@todo, add packet sniffing on the fly.
     run = 't'
     albert_faces()
-    time.sleep(0.4)
+    sleep(0.4)
     while run == 't':
         try:
             os.system(clear)
             options = str(input("[ + ] Would you like to use:\n"
                        "1.) Shodan\n"
                        "2.) Nmap(Targeted Scanning of host system written out to XML file)\n"
+                       "3.) Inactive(Zombie Host Scapy Scan)"
                        "->"))
             if options == '1':
                 os.system(clear)
@@ -150,6 +188,10 @@ if __name__ == '__main__':
                 host = str(input("[ + ] Please input host IP:\n->"))
                 port = str(input("[ + ] Please input port:\n->"))
                 nmapScan(host, port)
+            if options == "3":
+                choice = str(input("[ + ] Please input the subnet to detect [ + ]\n->"))
+                if choice != '':
+                    subnet_discover(choice)
             if options == '':
                 os.system(clear)
                 print("[ ! ] Please enter a value! [ ! ]")
