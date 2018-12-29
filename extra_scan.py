5ecd5e0b9d0382483bba3af9563e0993b03c4e07
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-12-29 13:51:23 -0500

    Update extra_scan.py

--- extra_scan.py
+++ extra_scan.py
@@ -1,32 +1,35 @@
-#this file is still being developed.
-
 def extras_scan():
     import os
-    from termcolor import cprint
+    from termcolor import cprint, colored
+    from pathlib import Path
+
     extras = list()
     persist = list()
-    print("[ + ] Available extras [ + ]")
-    for files in os.listdir('./data/scripts/'):
-        cprint("[ + ] {} [ + ]".format(files), "green")
-        for file in files:
-            if file.endswith('.py'):
-                extras.append(file)
-    print("[ + ] Persistance Modules: [ + ]")
-    for files in os.listdir('./data/scripts/persistance/'):
-        cprint("[ + ] {} [ + ]".format(files), "green")
-        for file in files:
-            if file.endswith(".py"):
-                persist.append(file)
-    try:
-        for entry in extras[0:]:
-            print("[ + ] Your choices are: \n -> {}".format(entry))
-        choice = int(input("Please enter your choice:\n->"))
-        if choice != '':
-            print(extras[choice])
-    except (ValueError, IndexError, TypeError) as e:
-        cprint("[ !! ] Incorrect Choice.. Try again. [ !! ]\n {}".format(e), "red")
-        pass
+    PATH = './data/scripts/'
+    PERSIST_SCRIPTS = './data/scripts/persistance'
+    decision = input("[ ! ] Please select a path:\n1 . ){} \n2 . ){}".format(PATH, PERSIST_SCRIPTS))
+    if decision == '1':
+        print("[ + ] Available extras [ + ]")
+        for files in os.listdir('./data/scripts/'):
+            cprint("[ + ] {} [ + ]".format(files), "white", attrs=['blink', 'bold'])
+            for file in files:
+                if file.endswith('.py'):
+                    extras.extend(file)
+        choice = input("Please enter your choice:\n->")
+        for choice in extras:
+            cprint("[ + ] You selected: [ + ]\n->{}".format(choice), "blue", attrs=["bold"])
+            Path(PATH + choice)
+
+    if decision == '2':
+        print("[ + ] Persistance Modules: [ + ]")
+        for files in os.listdir('./data/scripts/persistance/'):
+            cprint("[ + ] {} [ + ]".format(files), "white", attrs=['blink', 'bold'])
+            for file in files:
+                if file.endswith(".py"):
+                    persist.extend(file)
+        choice = input("Please enter your choice:\n->")
+        for choice in persist:
+            cprint("[ + ] You selected: [ + ]\n->{}".format(choice), "blue", attrs=["bold"])
 
 if __name__ == "__main__":
-    while True:
-        extras_scan()
+    extras_scan()
