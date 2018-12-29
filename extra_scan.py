38d5d33df58238889beabeaaa321bd4c2802de30
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-12-29 13:53:54 -0500

    Update extra_scan.py

--- extra_scan.py
+++ extra_scan.py
@@ -7,7 +7,7 @@ def extras_scan():
     persist = list()
     PATH = './data/scripts/'
     PERSIST_SCRIPTS = './data/scripts/persistance'
-    decision = input("[ ! ] Please select a path:\n1 . ){} \n2 . ){}".format(PATH, PERSIST_SCRIPTS))
+    decision = input("[ ! ] Please select a path:\n1 . ){} \n2 . ){}\n->".format(PATH, PERSIST_SCRIPTS))
     if decision == '1':
         print("[ + ] Available extras [ + ]")
         for files in os.listdir('./data/scripts/'):
@@ -16,9 +16,10 @@ def extras_scan():
                 if file.endswith('.py'):
                     extras.extend(file)
         choice = input("Please enter your choice:\n->")
-        for choice in extras:
-            cprint("[ + ] You selected: [ + ]\n->{}".format(choice), "blue", attrs=["bold"])
-            Path(PATH + choice)
+        for index in persist:
+            if choice == index:
+                cprint("[ + ] You selected: [ + ]\n->{}".format(choice), "blue", attrs=["bold"])
+                Path(PATH + choice)
 
     if decision == '2':
         print("[ + ] Persistance Modules: [ + ]")
@@ -28,8 +29,9 @@ def extras_scan():
                 if file.endswith(".py"):
                     persist.extend(file)
         choice = input("Please enter your choice:\n->")
-        for choice in persist:
-            cprint("[ + ] You selected: [ + ]\n->{}".format(choice), "blue", attrs=["bold"])
+        for index in persist:
+            if choice == index:
+                cprint("[ + ] You selected: [ + ]\n->{}".format(choice), "blue", attrs=["bold"])
 
 if __name__ == "__main__":
     extras_scan()
