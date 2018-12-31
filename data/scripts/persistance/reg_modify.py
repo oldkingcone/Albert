b78f59a6615c8571095d7b6e4a02fee64b1b6501
Chef Gordon R <43897033+ChefGordon@users.noreply.github.com>
2018-12-31 15:17:37 -0500

    Update reg_modify.py

--- data/scripts/persistance/reg_modify.py
+++ data/scripts/persistance/reg_modify.py
@@ -2,6 +2,6 @@
 import win32api as winapi
 
 try: 
-	print(winapi.GetCommandLine(" REG ADD HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce\\toteslegit"))
-	print(winapi.GetCommandLine("REG ADD HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce /v toteslegit /t REG_MULTI_SZ /f"))
+	print(winapi.GetCommandLine(" REG ADD HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce\\Albert"))
+	print(winapi.GetCommandLine("REG ADD HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce /v Albert /t REG_MULTI_SZ /f"))
 	
