03732882604747e626b1f17c5ae869eb2db4b506
Chef Gordon R <43897033+ChefGordon@users.noreply.github.com>
2018-12-31 15:16:58 -0500

    Update reg_modify.py

--- data/scripts/persistance/reg_modify.py
+++ data/scripts/persistance/reg_modify.py
@@ -3,5 +3,5 @@ import win32api as winapi
 
 try: 
 	print(winapi.GetCommandLine(" REG ADD HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce\\toteslegit"))
-	print(winapi.GetCommandLine("REG ADD HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce /v toteslegit /t REG_DWORD_LITTLE_EDIAN /f"))
+	print(winapi.GetCommandLine("REG ADD HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce /v toteslegit /t REG_MULTI_SZ /f"))
 	
