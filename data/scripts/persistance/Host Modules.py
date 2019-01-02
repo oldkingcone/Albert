3a4e2ec16545b7b87f7c3f38659c583dde089619
Chef Gordon R <43897033+ChefGordon@users.noreply.github.com>
2019-01-01 19:30:22 -0500

    Add files via upload

---
+++ data/scripts/persistance/Host Modules.py
@@ -0,0 +1,58 @@
+ # New Idea 
+
+ import win32api as winapi 
+
+ def CustomInput():
+
+ 	PATH_DIR = './data/scripts/persistance/'
+ 		with open(PATH_DIR, 'r') as a:
+ 	
+ try: 
+			style = a.readlines()
+			for line in style:
+				print(line.strip('\n'))
+				passes.add(line.strip('\n'))
+	options = str(input("\n\n\n\t[ + ] Hey! Hey! Hey! Choose An Exploit!\n" \
+						"\t1 . ) Powershell\n" \
+						"\t2 . ) Linux\n" \
+						"\t3 . ) LDAP\n" \
+						"\t4 . ) Active Directory [ + ]\n" \
+						"[ * ] - >"))
+	if choice == "1":
+
+ 		alberts = './art/albert_face_2.txt'
+ 		face = open(ablerts, "r")
+ 		imean = face.readlines()
+ 		for line in imean:
+ 			cprint(line.strip("\n"), 'blue')
+ 		cprint("Lets Put Some Power In That Shell!! ")
+ 			continue
+ 		cprint("Please Choose And Exploit\n" \                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+ 				"\t ---------------------------------------------------\n\n"
+ 					"Do note that you may also add your own moduels!\n"
+ 				"[ * ] - >")
+ 				
+ 				continue
+ 														#Powershell Module 
+ 										#scripts = './data/scripts/Modules/PowerShell'																				# Need to create a listing for all the available scripts for that specific modules 
+ 																						
+										#MODULE_DIR = './data/scripts/Modules/PowerShell/'										
+ 		Dur_Hur = "ps {}.format("MODULE_DIR")                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+ 		try:
+ 			print(winapi.GetCommandLine(Dur_Hur))
+
+ 	except Exception as e:
+ 		print("{-} Did Not Work:\n{} [~]".format(e))
+
+ 	except KeyboardInterrupt:
+ 		choice = str(input("[ + ] Would you like to exit? [ + ]\n->")).lower()
+ 		if choice != "y":
+ 				print("Ight Den")
+ 				return 
+ 		if choice == 'y':
+ 				print([!!] Cya! [!!]")
+ 					sys.exit(1)
+
+ 		      
