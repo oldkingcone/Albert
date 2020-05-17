b12205cda8f8568f94908e8fa1e71c73aac8bafe
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2020-05-17 12:20:35 -0400

    Add files via upload

---
+++ data/tools/scripts/domain/TalkShitGetHit2.sh
@@ -0,0 +1,126 @@
+#!bin/sh
+
+	echo -e "\e[1;94m======================================================================\e[m"
+		cat << "img"
+			-------------------------
+
+                    ███████ ]▄▄▄▄▄▄▄▄ _ _ _ _ _ _ _   "KURD A Cominnnn"
+               ▂▄▅█████████▅▄▃▂     ☻      \|/
+            Il███████████████████]    / ▌\╦─   - - - - - - - - 
+                @@@@@@@@@@@@@@         /\       |
+img                                             |
+                                                |
+	echo -e "\e[1;94m=====================================================================\e[m"
+
+
+
+
+
+string="fail"
+#Option 1 #
+# tail -n 0 -F /path/to/da/file | \ #  #Establish Path To Log File 
+#	while read LINE #
+#do#
+#	if echo "$LINE" | grep -q $string #
+#		if [ $? = 0 ] # 
+
+#Option 2
+tails=tail -fn0 /path/to/da/file | \   #Establish Path to log File 
+	while read -r line;				   #Prevent blacklash escapes from being interupted
+ do
+	echo "$line"
+		if [ $? = 0 ] 
+
+then
+
+	echo -e  "Looks like we got a crook! What to do what to do"
+then
+	echo -e "/e[1;94m=====================================================================================/e[m"
+          cat << "img2"
+                  /|  /|  -------------------------                               _------__--___.__.
+                ||__||  |                         |                             /            `  `    \
+               /   O O\__  KURD IN THIS BITCH     |                            |                      \      Butthead, 
+              /          \   Your OS Belongz      |                            |                       |	Do you understand?
+             /      \     \       To US           |                             \                      |
+            /   _    \     \ ----------------------                               ~/ --`-`-`-\         |                         
+           /    |\____\     \      ||                                             |            |       |
+          /     | | | |\____/      ||                                             |            |       |
+         /       \| | | |/ |     __||                                             |   _--    |       |
+        /  /  \   -------  |_____| ||                                             _| =-.    |.-.    |
+       /   |   |           |       --|                                            o|/o/       _.   |
+       |   |   |           |_____  --|                                             /  ~          \ |
+       |  |_|_|_|          |     \----                                           (____@)  ___~    |      
+       /\                  |                                                        |_===~~~.`    |  
+      / /\        |        /                                                     _______.--~     |    
+     / /  |       |       |                                                      \________       |
+ ___/ /   |       |       |                                                               \      |
+|____/    c_c_c_C/ \C_c_c_c                                                              _/-___-- -__
+                                                                                       /            __\ 
+                                                                                       /-| Metallica|| |
+                                                                                      / /|          || |
+                        
+                       
+img2
+	echo -e "\e[1;94m====================================================================================\e[m"
+
+	echo -e "Oh Shit I know what to do! Lets cook em!"
+		then
+				cat /path/to/list | while read output
+			do
+		ping -c 1 "$output" > /dev/null
+		if [ $? -eq 0 ]; then
+			echo "IP $output is up"
+		else
+			echo "IP $output is down"
+	fi
+		then	
+			echo -e "KURDDDD A COMMIN"
+		
+		then 
+			read -p "Save Scan as (XML): " file
+			read -p "Attack!: " $tails
+			echo $file
+
+		do
+			sudo nmap -sS -sV -p- --script=firewall-bypass --script=vulscan -A -O -T3 -Pn "$tails" -oX "$file"
+
+		then
+			echo -e "We INIT nigga"
+		then 
+			echo -e "Lets Use Another Script Aye?"
+		do 
+			# Insert New Payloads here 
+			echo -e "Yo Nikkuh We Ran Another Script Successfully!"
+	fi	
+		then 
+			host= $LINE
+			for i in {1..254}
+
+		do
+
+			hostname="$host"
+			if [ "$?" -eq 0 ]; 
+		then
+				echo -ne $i
+				echo -ne "/t"
+				echo -ne "PTR"
+				echo -ne "/t"
+				echo -ne "IN"
+				echo -ne "/t"
+			echo 'echo $hostname | cut -d " " -f 4'
+#############################################################################################################################
+
+ ______                  _______                   __           
+ |___  /                 |__   __|                 / _|          
+    / / ___  _ __   ___     | |_ __ __ _ _ __  ___| |_ ___ _ __  
+   / / / _ \| '_ \ / _ \    | | '__/ _` | '_ \/ __|  _/ _ \ '__| 
+  / /_| (_) | | | |  __/    | | | | (_| | | | \__ \ ||  __/ |    
+ /_____\___/|_| |_|\___|    |_|_|  \__,_|_| |_|___/_| \___|_|    
+
+ ###############################################################################################################################
+
+	
+
+	fi
+done
+
