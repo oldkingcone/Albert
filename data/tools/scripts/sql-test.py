a90952aee0cfd0a391ed353d06d31fa3dbd03168
Ameliorate <35051260+0x4meliorate@users.noreply.github.com>
2020-05-17 21:37:30 +0100

    Update sql-test.py

--- data/tools/scripts/sql-test.py
+++ data/tools/scripts/sql-test.py
@@ -3,11 +3,21 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+errors = [
+    '"=""'
+]
+
 def get(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
-    for item in soup.select('input'):
+    for form in soup.select('form'):
 
-        print(str(item['name']))
+        print(str(form['id']))
+        print(str(form['method']))
+        print(str(form['action']))
+
+        for inputs in soup.select('input'):
+            print(str(inputs['name']))
         
-get("http://0.0")
+
+get("127.0.0.1:8080")
