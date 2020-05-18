c81ec2a2daa1b02a74085133e2b18050e855637d
Ameliorate <35051260+0x4meliorate@users.noreply.github.com>
2020-05-18 12:33:49 +0100

    Update sql-test.py

--- data/tools/scripts/sql-test.py
+++ data/tools/scripts/sql-test.py
@@ -18,39 +18,35 @@ def get(url):
     # GET request
     res = session.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
-    prepare(soup.find("form"))
+    return soup.find_all("form")
 
-def prepare(form):
+def prepare(form, inject):
 
-    print(form)
+    payload = {}
 
-    # For each error based SQL input
-    for error in errors:
+    inputs = []
 
-        payload = {}
+    action = form.attrs.get("action")
+    method = form.attrs.get("method")
 
-        action = form.attrs.get("action")
-        method = form.attrs.get("method")
+    for input_tag in form.select("input"):
 
-        inputs = []
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        input_value = inject # Value
+        inputs.append({"type": input_type, "name": input_name, "value": input_value})
 
-        for input_tag in form.select("input"):
+    payload["action"] = action
+    payload["method"] = method
+    payload["inputs"] = inputs
 
-            input_type = input_tag.attrs.get("type", "text")
-            input_name = input_tag.attrs.get("name")
-            input_value = error
-            inputs.append({"type": input_type, "name": input_name, "value": input_value})
-
-            payload["action"] = action
-            payload["method"] = method
-            payload["inputs"] = inputs
-
-            print(payload)
-
-            return payload
-
-        else:
-            print("[SQL] >> No error based input defined?")
+    return payload
 
 
-get("http://0.0.0.0:6779")
+for error in errors:
+
+    forms = get("http://localhost:8080/sql.php")
+
+    for i, form in enumerate(forms, start=1):
+        form_details = prepare(form, error)
+        print(form_details)
