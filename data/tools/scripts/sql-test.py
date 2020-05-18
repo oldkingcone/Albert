71613a56da8408cba165f544160e7fa6be466061
Ameliorate <35051260+0x4meliorate@users.noreply.github.com>
2020-05-18 01:44:55 +0100

    Update sql-test.py

--- data/tools/scripts/sql-test.py
+++ data/tools/scripts/sql-test.py
@@ -1,5 +1,6 @@
 # -- coding: utf-8 --
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 import requests
 import re
 
@@ -11,44 +12,34 @@ errors = [
     "test"
 ]
 
-s = requests.Session()
+inputs = []
+
+session = HTMLSession()
 
 def get(url):
-    # Pull page contents
-    page = s.get(url)
-    # Parse contents
-    soup = BeautifulSoup(page.text, "html.parser")
-    # For each form
-    for forms in soup.select('form'):
+    # GET request
+    res = session.get(url)
+    soup = BeautifulSoup(res.html.html, "html.parser")
+    prepare(soup.find_all("form"))
+
+def prepare(form):
+
+    print(form)
+
+    payload = {}
+
+    action = form.get("action").lower()
+    method = form.get("method").lower()
+
+    inputs = []
+
+    for input_tag in form.find_all("input"):
         # For each error based SQL input
         for error in errors:
-            # For each input within each form
-            for inputs in soup.select('input'):
-                # Check if method is GET.
-                if str(forms['method']).lower() == "get":
-                        
-                    print("[SQL] >> GET REQUEST - " + str(forms['action'] + "?" + str(inputs.get('name')) + "=" + error))
 
-                    # Checks if action is the base URL, in order to parse the request URL
-                    if str(forms['action']).startswith("http://") or str(forms['action']).startswith("https://"):
-                        # GET request for each parameter with each input
-                        r = s.get(str(forms['action'] + "?" + str(inputs.get('name')) + "=" + error))
-                        # Handle output
-                        output = BeautifulSoup(r.text, "html.parser")
-
-                    else:
-                        # GET request for each parameter with each input
-                        r = s.get(url + str(forms['action'] + "?" + str(inputs.get('name')) + "=" + error))
-                        # Handle output
-                        output = BeautifulSoup(r.text, "html.parser")
-
-                # Checks if method is POST
-                elif str(forms['method']).lower() == "post":
-                    # POST request for each parameter with each input
-                    s.post(url, data={str(inputs.get('name')) : error})
-
-                # Checks if no method was found and throws a response
-                else:
-                    print("[SQL] >> Method for Form: " + str(forms['id']) + " could not be found?")   
+            print(error)
+        
+        else:
+            print("[SQL] >> No error based input defined?")
 
 get("http://0.0.0.0:6779")
