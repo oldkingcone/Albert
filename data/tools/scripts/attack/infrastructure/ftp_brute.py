43630eb2426ccb69d337ac1da0daa6b0dd3c1ca7
oldkingcone <no.no@no.com>
2021-03-21 13:41:30 -0400

    quality of life
    starting build-out of ftp logic, corrected the requirements to requirements.txt

--- data/tools/scripts/attack/infrastructure/ftp_brute.py
+++ data/tools/scripts/attack/infrastructure/ftp_brute.py
@@ -1,4 +1,5 @@
 from ftplib import FTP, FTP_TLS
+import pefile
 
 
 def brute(host, port, uplist):
@@ -10,13 +11,16 @@ def brute(host, port, uplist):
                     print("Wow, so anonymous login is allowed, double check to make sure this IS NOT a honeypot.")
                 for index in uplist:
                     if "230" in ftpBrute.login(user=index[0], passwd=index[1]):
-                        print(f"Identified: {index} as a valid login.")
+                        print(f"Identified: {index}:{index[1]} as a valid login.")
                     else:
                         continue
 
 
-def uploader(host, port, username, password):
-    print("Coming soon...")
+def uploader(host, port, username, password, whattoupload, os):
+    if isinstance(host, str) and isinstance(port, int) and isinstance(username, str) and isinstance(password, str):
+        if isinstance(whattoupload, str) and "win" in str(os).lower():
+            print("Ensuring that the upload is PE/PS1/BAT/VBS or other Windows based file types.")
+
 
 
 def ftp_proxy(host, port, username, password):
